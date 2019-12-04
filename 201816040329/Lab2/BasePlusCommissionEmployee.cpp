f38e8d227f6a2b86736b50b450540179a5365b81
kennnna <56779636+201816040329@users.noreply.github.com>
2019-12-04 16:50:58 +0800

    Create BasePlusCommissionEmployee.cpp

---
+++ 201816040329/Lab2/BasePlusCommissionEmployee.cpp
@@ -0,0 +1,123 @@
+// Lab 2: BasePlusCommissionEmployee.cpp
+// Member-function definitions of class BasePlusCommissionEmployee
+// using composition.
+#include <iostream>
+using namespace std;
+
+// BasePlusCommissionEmployee class definition
+#include "BasePlusCommissionEmployee.h"
+#include"CommissionEmployee.h"
+
+// constructor
+BasePlusCommissionEmployee::BasePlusCommissionEmployee(
+   const string &first, const string &last, const string &ssn,
+   double sales, double rate, double salary )
+   // initialize composed object
+:k(first,last,ssn,sales,rate)
+       /* Initialize the commissionEmployee data member,
+	    pass (first, last, ssn, sales, rate) to its constructor */
+{
+   setBaseSalary( salary ); // validate and store base salary
+} // end BasePlusCommissionEmployee constructor
+
+// set commission employee's first name
+void BasePlusCommissionEmployee::setFirstName( const string &first )
+{
+   /* Call commissionEmployee's setFirstName function */
+ setFirstName(first);
+} // end function setFirstName
+
+// return commission employee's first name
+string BasePlusCommissionEmployee::getFirstName() const
+{
+   /* Call commissionEmployee's getFirstName function */
+   return k.getFirstName();
+} // end function getFirstName
+
+// set commission employee's last name
+void BasePlusCommissionEmployee::setLastName( const string &last )
+{
+   /* Call commissionEmployee's setLastName function */
+   setLastName(last);
+} // end function setLastName
+
+// return commission employee's last name
+string BasePlusCommissionEmployee::getLastName() const
+{
+   /* Call commissionEmployee's getLastName function */
+
+    return k.getLastName();
+} // end function getLastName
+
+// set commission employee's social security number
+void BasePlusCommissionEmployee::setSocialSecurityNumber(
+   const string &ssn )
+{
+   /* Call commissionEmployee's setSocialSecurity function */
+   setSocialSecurityNumber(ssn);
+} // end function setSocialSecurityNumber
+
+// return commission employee's social security number
+string BasePlusCommissionEmployee::getSocialSecurityNumber() const
+{
+   /* Call commissionEmployee's getSocialSecurity function */
+   return k.getSocialSecurityNumber();
+} // end function getSocialSecurityNumber
+
+// set commission employee's gross sales amount
+void BasePlusCommissionEmployee::setGrossSales( double sales )
+{
+   /* Call commissionEmployee's setGrossSales function */
+   setGrossSales(sales);
+} // end function setGrossSales
+
+// return commission employee's gross sales amount
+double BasePlusCommissionEmployee::getGrossSales() const
+{
+   /* Call commissionEmployee's getGrossSales function */
+   return k.getGrossSales();
+} // end function getGrossSales
+
+// set commission employee's commission rate
+void BasePlusCommissionEmployee::setCommissionRate( double rate )
+{
+  setCommissionRate(rate);
+} // end function setCommissionRate
+
+// return commission employee's commission rate
+double BasePlusCommissionEmployee::getCommissionRate() const
+{
+   /* Call commissionEmployee's getCommissionRate function */
+  return k.getCommissionRate();
+} // end function getCommissionRate
+
+// set base salary
+void BasePlusCommissionEmployee::setBaseSalary( double salary )
+{
+   baseSalary = ( salary < 0.0 ) ? 0.0 : salary;
+} // end function setBaseSalary
+
+// return base salary
+double BasePlusCommissionEmployee::getBaseSalary() const
+{
+   return baseSalary;
+} // end function getBaseSalary
+
+// calculate earnings
+double BasePlusCommissionEmployee::earnings() const
+{
+   return getBaseSalary() +k.earnings();
+      /* Call commissionEmployee's earnings function */;
+} // end function earnings
+
+// print BasePlusCommissionEmployee object
+void BasePlusCommissionEmployee::print() const
+{
+   cout << "base-salaried ";
+
+   // invoke composed CommissionEmployee object's print function
+   /* Call commissionEmployee's print function */
+   k.print();
+
+   cout << "\nbase salary: " << getBaseSalary();
+} // end function print
