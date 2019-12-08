089c8e805caa39ee1cd0d6d8cc97a771d266566d
zhouyuxaing <56668463+zhouyuxaing@users.noreply.github.com>
2019-12-08 17:46:23 +0800

    Create SavingAccount.h
    This is  my lab-03;
    student ID:201816040319

---
+++ 201816040319/Lab1/SavingAccount.h
@@ -0,0 +1,25 @@
+/ Lab 3: SavingsAccount.h
+// Definition of SavingsAccount class.
+#ifndef SAVINGS_H
+#define SAVINGS_H
+
+#include "Account.h"
+/* Write a directive to include the Account header file */
+
+/* Write a line to have class SavingsAccount inherit publicly from Account */
+class SavingsAccount: public Account
+{
+public:
+   // constructor initializes balance and interest rate
+   SavingsAccount( double = 0, double = 0);
+   /* Declare a two-parameter constructor for SavingsAccount */
+
+    double calculateInterest();
+   /* Declare member function calculateInterest */
+private:
+    double interestRate;
+   /* Declare data member interestRate */
+}; // end class SavingsAccount
+
+#endif
+
